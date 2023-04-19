02dfe84417dd8dfaa256acdfe5eca2538e114d42
Khalid Supreme <richunclekhalid@gmail.com>
2023-04-19 13:10:48 +0100

    use conditionals and boolean
    I used conditional statements and Boolean logic in this code, and also introduced a new method of echo '<?= *** ?>.

--- index.php
+++ index.php
@@ -1,19 +1,40 @@
-<!-- I will be posting my code along from a from an online tutorial on 'php' to keep my fingers moist 😉, thanks. let's go!! -->
+<!-- I will be posting my code-along from a from an online tutorial on 'php' to keep my fingers moist 😉, thanks. let's go!! -->
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>php -zero</title>
+    <style>
+        body {
+            place-items: center;
+            display: grid;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
+
 <body>
+    <?php
+    $food = "Jollof Rice and Plantain";
+    $hungry = true;
+
+    if($hungry){
+        $order = "I want to eat $food";
+    }else{
+        $order = "I am not hungry";
+    }
+
+
+
+    ?>
+
     <h1>
-        <?php 
-        $Intro = "My name is ";
-        // echo $Intro . 'Supreme.';
-        echo "$Intro Supreme";
+        <?= $order ?>
         
-        
-        ?> 
     </h1>
 </body>
+
 </html>
